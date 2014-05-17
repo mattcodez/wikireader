3e315dda8061ca4017240f93000682881120f989
Matthew Molnar <matthew.molnar@gmail.com>
2014-05-17 02:30:43 -0400

    Basic coloring of letter

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function(){
 	var wordString = 'A computer is a general purpose device that can be programmed to carry out a set of arithmetic or logical operations automatically. Since a sequence of operations can be readily changed, the computer can solve more than one kind of problem.';
 	var words = wordString.split(' ');
 	
-	var wordSpeed = 200;
+	var wordSpeed = 500;
 	var wordIndex = 0;
 	function nextWord(){
 		if (wordIndex >= words.length){
@@ -10,13 +10,21 @@ $(function(){
 			return;
 		}
 		
-		setWord(words[++wordIndex]);
+		setWord(words[wordIndex++]);
 	};
 	
 	var wordTick = setInterval(nextWord, wordSpeed);
 	
 	var wordDisplay = $('#wordDisplay');
+	var wordSections = wordDisplay.find('span');
 	function setWord(word){
-		wordDisplay.text(word);
+		var redIndex = Math.floor(word.length / 2) - 1;
+		var first = word.slice(0, redIndex);
+		var middle = word[redIndex];
+		var end = word.slice(redIndex + 1);
+		
+		wordSections.eq(0).text(first);
+		wordSections.eq(1).text(middle);
+		wordSections.eq(2).text(end);
 	}
 });
