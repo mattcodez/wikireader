eb55a37909b8194cad4d0c314371d8046a951655
Matthew Molnar <matthew.molnar@gmail.com>
2014-05-18 03:15:15 -0400

    Modify speed with arrow keys.

--- js/main.js
+++ js/main.js
@@ -27,4 +27,32 @@ $(function(){
 		wordSections.eq(1).text(middle);
 		wordSections.eq(2).text(end);
 	}
+	
+	var wpmHolder = $('h2 span');
+	function setWPMTitle(wordSpeed){
+		wpmHolder.text((1000 / wordSpeed) * 60);
+	}
+	
+	setWPMTitle(wordSpeed);
+	
+	$(document.body).on('keyup', function(e){
+		var mod = 0;
+		switch (e.which) {
+			case 38: //up
+				mod = -50;
+			break;
+			
+			case 40: //down
+				mod = 50;
+			break;
+		}
+		
+		if (mod){
+			clearInterval(wordTick);
+			wordSpeed += mod;
+			if (wordSpeed < mod) wordSpeed = mod;
+			wordTick = setInterval(nextWord, wordSpeed);
+			setWPMTitle(wordSpeed);
+		}
+	});
 });
